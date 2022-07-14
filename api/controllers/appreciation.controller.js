c0f995a729284f42294a58d92fefe15e64ed14a8
Aman Singh Bhandari <am727005@dal.ca>
2022-07-14 13:34:41 -0300

    get appreciation

--- api/controllers/appreciation.controller.js
+++ api/controllers/appreciation.controller.js
@@ -15,6 +15,24 @@ const updateAppreciation = async (req, res) => {
   }
 };
 
+// Get a post based on the id
+const getAppreciation = async (req, res) => {
+  const { userid } = req.params;
+  try {
+    const appreciation = await appreciationService.getAppreciation(userid);
+
+    res.status(200).json({
+      message: "ok",
+      appreciation,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
+
 export const appreciationController = {
   updateAppreciation,
+  getAppreciation,
 };
