33878cb9b0f7f2d44788e5eba82edc849a30fec3
Naga Pavan Srivathsav Chevuru <ng490417@dal.ca>
2022-07-15 00:54:53 -0300

    Updated APIs

--- api/routes/posts.route.js
+++ api/routes/posts.route.js
@@ -3,8 +3,9 @@ import { postsController } from "../controllers/index.js";
 
 const postsRoute = Router();
 
-postsRoute.get("/", postsController.postsGET);
-postsRoute.get("/:id", postsController.postsByIDGET);
+postsRoute.get("/:id", postsController.postsGET);
 postsRoute.post("/savePost", postsController.postsPOST);
+postsRoute.delete("/deletePost/:id", postsController.postsByIDDELETE);
+postsRoute.put("/putPost/:id", postsController.postsByIDPUT);
 
 export default postsRoute;
