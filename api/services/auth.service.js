6740aaccaefd0bd37ae53dabf961b70e0faba69e
Kuldeep Bhimani <kl681090@dal.ca>
2022-07-17 00:10:45 -0300

    registration fixed

--- api/services/auth.service.js
+++ api/services/auth.service.js
@@ -37,6 +37,8 @@ const registerService = async (
   res
 ) => {
   const savedUser = await AuthUser.findOne({ email });
+  const token = jwt.sign(email, "Kuldeep");
+
   if (savedUser) {
     res.status(422).json({
       message: "user already exist",
@@ -71,6 +73,7 @@ const registerService = async (
         res.status(200).json({
           message: "OK",
           data,
+          token,
         });
         console.log(data);
       }
